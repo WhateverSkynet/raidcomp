03e3a6753de79f2be53dc554a5c1b19ba5387e5e
Dāvis Voiceščuks <davis.voicescuks@gmail.com>
2017-11-10 21:32:27 +0200

    Fix failing test

--- server/test/services/character.test.js
+++ server/test/services/character.test.js
@@ -8,12 +8,4 @@ describe('\'character\' service', () => {
 
     assert.ok(service, 'Registered the service')
   })
-
-  it('updates syncs character with Blizzard api', async () => {
-    const service = app.service('/api/character')
-
-    const character = await service.sync(null, { name: 'Bliezeja', realm: 'Auchindoun', region: 'eu' })
-
-    assert.ok(character, 'Character synced')
-  })
 })
